90c76f03669e9efff358546fe7ee59f41c485898
Viano Di Napoli <vianodinapoli@gmail.com>
2026-03-10 16:54:49 +0200

    design system ajustado

--- maintenance-system/resources/views/layouts/guest.blade.php
+++ maintenance-system/resources/views/layouts/guest.blade.php
@@ -17,12 +17,18 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
              <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <!-- <a href="{{ route('dashboard') }}">
                         {{-- Pode usar um componente de Logo ou o seu logo da Porto Cargas --}}
                         <img decoding="async" 
                              src="https://fem.co.mz/wp-content/uploads/2023/03/logo_pequeno.png" 
                              class="h-13 w-auto fill-current text-gray-800" alt="{{ config('app.name', 'Laravel') }}">
-                    </a>
+                    </a> -->
+<a href="{{ route('dashboard')}}">
+    <img src="{{ asset('images/bymozelogo.png') }}" 
+         {{ $attributes }}
+         class="h-7 w-auto object-contain" 
+         alt="Logo Bymoze">
+</a>
                 </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
