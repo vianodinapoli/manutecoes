7aa7e07e0eb6ee5421287b7083b5a94b8c7656f7
Viano Di Napoli <vianodinapoli@gmail.com>
2026-03-12 16:07:59 +0200

    remoção do botão criar conta no login

--- maintenance-system/resources/views/auth/login.blade.php
+++ maintenance-system/resources/views/auth/login.blade.php
@@ -34,7 +34,7 @@
 
         <div class="flex items-center justify-end mt-4">
             
-            @if (Route::has('password.request'))
+            <!-- @if (Route::has('password.request'))
                 {{-- <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a> --}}
@@ -44,7 +44,7 @@
                                 href="{{ route('register') }}"
                                 class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
                                 Criar conta
-                            </a>
+                            </a> -->
 
             <x-primary-button class="ms-3">
                 {{ __('Entrar') }}
